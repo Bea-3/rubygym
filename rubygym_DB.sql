67e4548258b679f310c0b6495b072116af01461e
beatrice <beatrice.pepple@gmail.com>
2023-09-15 19:50:18 +0100

    python scripts done, prior to hosting

---
+++ rubygym_DB.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 15, 2023 at 08:44 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rubygym`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `client_id` int(11) NOT NULL,
+  `client_fname` varchar(75) NOT NULL,
+  `client_lname` varchar(75) NOT NULL,
+  `client_phone` varchar(25) NOT NULL,
+  `client_address` varchar(255) DEFAULT NULL,
+  `client_email` varchar(100) DEFAULT NULL,
+  `client_pwd` varchar(100) DEFAULT NULL,
+  `client_subscription` enum('monthly','3 months','6 months','annually') NOT NULL DEFAULT 'monthly',
+  `program` enum('self','group','private training') NOT NULL DEFAULT 'self',
+  `client_regdate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`client_id`, `client_fname`, `client_lname`, `client_phone`, `client_address`, `client_email`, `client_pwd`, `client_subscription`, `program`, `client_regdate`) VALUES
+(6, 'Lizzy', 'Asa', '08123456789', '16 Gimpo lagos', 'lizzy@asa.com', 'pbkdf2:sha256:260000$urxtGClublQcH0sq$06875720727b729c95ce82322e890cb6b9e95574202bcca9c09a60d773eacd', '3 months', 'self', '2023-09-15 18:40:22'),
+(7, 'Princess', 'Nwosu', '08123451189', '72 church busstop osun', 'princess@yahoo.com', 'pbkdf2:sha256:260000$SocrlHvm5IFCeKbD$acd5715d284fd9e431aba3cb370f4c2f8e82cd405c5d13cf84d8396f198c4d', 'annually', 'private training', '2023-09-15 18:42:20'),
+(8, 'John', 'Oko', '08123456789', '15 johnson street lagos', 'john@oko.com', 'pbkdf2:sha256:260000$MW36KFnc2O31kUvR$8197a385b299640bc1e9742932fbb1280af8f5ac536d202a5d627f46759d9e', '6 months', 'group', '2023-09-15 18:42:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_contact`
+--
+
+CREATE TABLE `emergency_contact` (
+  `emcontact_id` int(11) NOT NULL,
+  `em_fname` varchar(75) NOT NULL,
+  `em_lname` varchar(75) NOT NULL,
+  `em_phonenum` varchar(25) NOT NULL,
+  `em_address` varchar(255) DEFAULT NULL,
+  `clientID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `emergency_contact`
+--
+
+INSERT INTO `emergency_contact` (`emcontact_id`, `em_fname`, `em_lname`, `em_phonenum`, `em_address`, `clientID`) VALUES
+(6, 'Jude', 'Miracle', '08234567890', '50 orange town ikeja', 6),
+(7, 'Blessing', 'Nwosu', '0812345678', '72 church busstop osun', 7),
+(8, 'Dexter', 'George', '08234567890', '50 blue street ikeja', 8);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`client_id`),
+  ADD UNIQUE KEY `client_email` (`client_email`),
+  ADD UNIQUE KEY `client_pwd` (`client_pwd`);
+
+--
+-- Indexes for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  ADD PRIMARY KEY (`emcontact_id`),
+  ADD KEY `clientID` (`clientID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  MODIFY `emcontact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  ADD CONSTRAINT `emergency_contact_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`client_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- rubygymapp/clientroutes.py
+++ rubygymapp/clientroutes.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask,request,session,jsonify
 from datetime import datetime
 from sqlalchemy import or_
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 #import local files
 from rubygymapp import app,db
@@ -12,91 +12,117 @@ from rubygymapp.models import Client,Emergency_contact
 def homme():
     return "home, please use the URL"
 
-@app.route('/user')
-def user():
-    fname = "Lizzy"
-    lname = "Asa"
-    phone = "08123456789"
-    add = "16 Gimpo lagos"
-    email = "lizzy@asa.com"
-    pwd = "liz123"
-    sub = "3 months"
-    prog = "self"
+@app.route('/api/adduser', methods=['POST'])
+def adduser():
+    data = request.get_json()
+
+    pwd_hash = generate_password_hash(data['password'])
+
+    fname = data['first_name']
+    lname = data['last_name']
+    phone = data['phone']
+    add = data['address']
+    email = data['email']
+    pwd = pwd_hash
+    sub = data['subscription']
+    prog = data['program']
     #emergency contact info
-    firstname = "Jude"
-    lastname ="Miracle"
-    pnum = "08234567890"
-    address = "50 orange town ikeja"
+    firstname = data['emergency_contact_first_name']
+    lastname = data['emergency_contact_last_name']
+    pnum = data['emergency_contact_phone']
+    address = data['emergency_contact_address']
 
 
-    cdata = Client(client_fname=fname, client_lname=lname, client_phone=phone, client_address=add, client_email=email, client_pwd=pwd,client_subscription=sub, program=prog )
-    db.session.add(cdata)
+    newuser = Client(client_fname=fname, client_lname=lname, client_phone=phone, client_address=add, client_email=email, client_pwd=pwd,client_subscription=sub, program=prog )
+    db.session.add(newuser)
     db.session.commit()
     #saving clientid as the session id
-    userid = cdata.client_id
+    userid = newuser.client_id
     session['user']=userid
     #populating the emergency contact table
     edata = Emergency_contact(em_fname = firstname, em_lname= lastname, em_phonenum = pnum, em_address = address, clientID=session['user'])
     db.session.add(edata)
     db.session.commit()
-    return "done inserting"
 
-#delete a client and all information will be deleted
-@app.route('/delete/api/<userid>')
-def deleteuser(userid):
-    userid = session.get('user')
-    if userid == None:
-        return "no session detected"
-    else:
-        #retrieve client id and delete
-        getuser = Client.query.get_or_404(userid)
-        db.session.delete(getuser)    
-        db.session.commit()
-        return "user"
+    return jsonify({'message' : 'New user created'})
+
+
+
+#fetch all users
+@app.route('/api/users', methods=['GET'])
+def get_all_users():
+    allusers = Client.query.join(Emergency_contact).add_columns(Emergency_contact).all()
+
+    output =[]
+
+    for user, info in allusers:
+        user_data = {}
+        user_data['id'] = user.client_id
+        user_data['first_name'] = user.client_fname
+        user_data['last_name'] = user.client_lname
+        user_data['phone'] = user.client_phone
+        user_data['address'] = user.client_address
+        user_data['email'] = user.client_email
+        user_data['password'] = user.client_pwd
+        user_data['subscription'] = user.client_subscription
+        user_data['program'] = user.program
+        user_data['emergency_contact_first_name'] = info.em_fname
+        user_data['emergency_contact_last_name'] = info.em_lname
+        user_data['emergency_contact_phone'] = info.em_phonenum
+        user_data['emergency_contact_address'] = info.em_address
+        output.append(user_data)
     
+    return jsonify({'users' : output})
+
+
 #retrieve db info and display as json
-@app.route('/fetch/api/<info>')
-def fetchuser(info):
-    data = Client.query.join(Emergency_contact).add_columns(Emergency_contact).filter(or_(Client.client_id==info,Client.client_fname==info,Client.client_lname==info)).all()
-    user_data = [{
-        'id': x.client_id, 
-        'first_name': x.client_fname, 
-        'last_name': x.client_lname,
-        'phone': x.client_phone,
-        'address': x.client_address,
-        'email': x.client_email,
-        'subscription': x.client_subscription,
-        'program': x.program,
-        'emergency_contact_first_name': y.em_fname,
-        'emergency_contact_last_name': y.em_lname,
-        'emergency_contact_phone': y.em_phonenum,
-        'emergency_contact_address': y.em_address,
-        } for x,y in data ]
-    return json.dumps(user_data, indent=4)
+@app.route('/api/user/<user_id>', methods=['GET'])
+def fetchuser(user_id):
+    # data = Client.query.join(Emergency_contact).add_columns(Emergency_contact).filter(or_(Client.client_id==info,Client.client_fname==info,Client.client_lname==info)).all()
+    data = Client.query.join(Emergency_contact).add_columns(Emergency_contact).filter(Client.client_id==user_id).all()
+
+    if not data:
+        return jsonify({'message' : 'No user found!'})
+    else:      
+        user_data = [{
+            'id': x.client_id, 
+            'first_name': x.client_fname, 
+            'last_name': x.client_lname,
+            'phone': x.client_phone,
+            'address': x.client_address,
+            'email': x.client_email,
+            'password': x.client_pwd,
+            'subscription': x.client_subscription,
+            'program': x.program,
+            'emergency_contact_first_name': y.em_fname,
+            'emergency_contact_last_name': y.em_lname,
+            'emergency_contact_phone': y.em_phonenum,
+            'emergency_contact_address': y.em_address,
+            } for x,y in data ]
+        return json.dumps(user_data, indent=4)
     
 #update client information
-@app.route('/update/api/<userid>')
+@app.route('/api/update/<userid>', methods = ["PUT"])
 def updateuser(userid):
-    userid = session.get('user')
-    if userid ==None:
-        return "no session detected"
+    #query client table, where the id is userid and check if id exists
+    getuser = Client.query.filter(Client.client_id==userid).first()
+    
+    if not getuser:
+        return jsonify({'message' : 'No user found!'})
     else:
-        #query client table, where the id is userid and update any information
-        add = request.args.get('client_address')
-        email = request.args.get('client_email')
-        pwd = request.args.get('client_pwd')
-        sub = request.args.get('client_subscription')
-        prog = request.args.get('program')
-        em_fname = request.args.get('em_fname')
-        em_lname = request.args.get('em_lname')
-        em_phone = request.args.get('em_phonenum')
-        em_add = request.args.get('em_address')
-        if add != '' and email != '' and pwd != '' and sub != '' and prog != '' and em_fname != '' and em_lname and em_phone and em_add != '' :
+        data = request.get_json()        
+
+        add = data['address']
+        sub = data['subscription']
+        prog = data['program']
+        em_fname = data['emergency_contact_first_name']
+        em_lname = data['emergency_contact_last_name']
+        em_phone = data['emergency_contact_phone']
+        em_add = data['emergency_contact_address']
+        if add != '' and sub != '' and prog != '' and em_fname != '' and em_lname and em_phone and em_add != '' :
             #query an object of EmergencyContact and Client ,get id, assign attributes, commit
             cobj = db.session.query(Client).get(userid)
             cobj.client_address = add
-            cobj.client_email = email
-            cobj.client_pwd = pwd
             cobj.client_subscription = sub
             cobj.program = prog
             cobj.emergency_det.em_fname = em_fname
@@ -104,10 +130,21 @@ def updateuser(userid):
             cobj.emergency_det.em_phonenum = em_phone
             cobj.emergency_det.em_address = em_add
             db.session.commit()
-            return "updated"
+            return jsonify({'message' : 'User Information Updated'})
         else:
-            return "please supply information"
+            return jsonify({'message' : 'Please supply information'})
 
 
-
+#delete a client and all information will be deleted
+@app.route('/api/delete/<userid>', methods = ["DELETE"])
+def deleteuser(userid):
+    getuser = Client.query.filter(Client.client_id==userid).first()
+    if not getuser:
+        return jsonify({'message' : 'No user found!'})
+    else:
+        #retrieve client id and delete
+        # user = Client.query.get_or_404(userid)
+        db.session.delete(getuser)    
+        db.session.commit()
+        return jsonify({'message' : 'User has been deleted'})
         
